43aee78baeeaea5108e9d5a99aa10e84732c3735
MikiKru <michal_kruczkowski@o2.pl>
2020-01-08 18:58:26 +0100

    j_adv_practice v10

--- src/main/java/threads/ThreadController.java
+++ src/main/java/threads/ThreadController.java
@@ -5,16 +5,23 @@ import java.util.Arrays;
 public class ThreadController extends Thread {
     @Override
     public void run(){
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 100; i++){
             System.out.println(Thread.currentThread().getName() + " : " + i);
         }
     }
 
     public static void main(String[] args) {
         String [] names = {"Ala", "Ola", "Ela", "Adam", "Jan"};
-        ThreadController tc = new ThreadController();
-        tc.start();
         Arrays.stream(names).forEach(System.out::println);
+        ThreadController tc1 = new ThreadController();
+        ThreadController tc2 = new ThreadController();
+        ThreadController tc3 = new ThreadController();
+        ThreadController tc4 = new ThreadController();
+        tc1.start();
+        tc2.start();
+        tc3.start();
+        tc4.start();
+
 
     }
 }
