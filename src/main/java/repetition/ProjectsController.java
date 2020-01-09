3dbfef9cef3627b7a2dc5a5b6b37cdb42806399c
MikiKru <michal_kruczkowski@o2.pl>
2020-01-09 19:03:05 +0100

    repetition -v7

--- src/main/java/repetition/ProjectsController.java
+++ src/main/java/repetition/ProjectsController.java
@@ -1,6 +1,7 @@
 package repetition;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 // Wykorzystujemy STRAEAM API LAMBDA I OPTIONAL
@@ -19,16 +20,19 @@ public class ProjectsController implements Projects {
     }
     // metoda zwracająca projekty posortowane po budżecie min-max tylko kategorii IT i Engineering
     public List<ProjectsFounds> getProjectsOrderByBudgetASCWithCategoryItAndEngineering(){
-        return 
+        return projects.stream()
+                .filter(projectsFounds -> projectsFounds.getCategory().equals("IT") || projectsFounds.getCategory().equals("Engineering"))  // filtrujemy po kategoriach
+                .sorted(Comparator.comparing(ProjectsFounds::getBudget))                                    // sortujemy po budżecie
+                .collect(Collectors.toList());                                                              //zwracamy listę
     }
 
     public static void main(String[] args) {
         ProjectsController pc = new ProjectsController();
-        pc.getProjectsOrderByBudgetDESC().stream()
-                .forEach(projectsFounds -> System.out.printf("Acronim: %-25s Budget: %20.2f PLN \n",
-                        projectsFounds.getAcronim(),
-                        projectsFounds.getBudget()));
+//        pc.getProjectsOrderByBudgetDESC().stream()
+//                .forEach(projectsFounds -> System.out.printf("Acronim: %-25s Budget: %20.2f PLN \n",
+//                        projectsFounds.getAcronim(),
+//                        projectsFounds.getBudget()));
         //        pc.getAllProjects();
-
+                    pc.getProjectsOrderByBudgetASCWithCategoryItAndEngineering().forEach(System.out::println);
     }
 }
