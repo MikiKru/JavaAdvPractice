14756e87723fcd7812de7a3bd8003dfb4bbe798b
MikiKru <michal_kruczkowski@o2.pl>
2020-01-09 20:27:03 +0100

    repetition -v9

--- src/main/java/repetition/ProjectsController.java
+++ src/main/java/repetition/ProjectsController.java
@@ -1,6 +1,8 @@
 package repetition;
+import java.text.NumberFormat;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -39,17 +41,48 @@ public class ProjectsController implements Projects {
                 .get();                                                             // get pobiera wartośc z Optionala
     }
     // metoda zwracająca projekt z najkrótszym czasem trwania
-
+    public ProjectsFounds getProjectWithMinTimeline(){
+        return projects.stream()
+                .min(Comparator.comparing(ProjectsFounds::getNumberOfMonths))
+                .get();
+    }
+    // metoda zwracająca średni czas trwania projektu
+    public Double getAverageProjectsTimelines(){
+        return projects.stream()
+                .mapToDouble(ProjectsFounds::getNumberOfMonths)     // bierzemy z obiektu projektu tylko miesiące
+                .average()                                          // liczymy wartość średnią -> Optional
+                .getAsDouble();                                     // pobieramy wartości z Optionala
+    }
     // metoda zwracająca całkowitą kwotę dofinansowania dla wszystkich projektów
-
-
+    public Double sumAllProjectFounds(){
+        return projects.stream()
+                .mapToDouble(ProjectsFounds::getFoundByProject)     // mapujemy obiekt na liczbę
+                .sum();                                             // sumujemy wszystkie liczby
+    }
+    // ile jest projektów w kategori podajen w argumencie funkcji
+    public Integer countProjectsByCategory(String category){
+        return (int) projects.stream()
+                    .filter(projectsFounds -> projectsFounds.getCategory().equals(category))
+                    .count();
+    }
 
     public static void main(String[] args) {
+        Locale locale = new Locale("en", "PL");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+
         ProjectsController pc = new ProjectsController();
-        System.out.printf("%s %.2fPLN %.2fPLN\n",
-                pc.getProjectWithMaxFound2().getAcronim(),
-                pc.getProjectWithMaxFound2().getBudget(),
-                pc.getProjectWithMaxFound2().getFoundByProject());
+        String category = "Education";
+        System.out.printf("Number of projects with category %s: %d", category, pc.countProjectsByCategory(category));
+//        System.out.println("Całkowite dofinansowanie projektów: " +
+//                currencyFormatter.format(pc.sumAllProjectFounds()).replace("PLN", "").replace(",", " ") + " PLN");
+        //        System.out.printf("Project timeline: %.1f\n", pc.getAverageProjectsTimelines());
+//        System.out.printf("%s %d",
+//                pc.getProjectWithMinTimeline().getAcronim(),
+//                pc.getProjectWithMinTimeline().getNumberOfMonths());
+//        System.out.printf("%s %.2fPLN %.2fPLN\n",
+//                pc.getProjectWithMaxFound2().getAcronim(),
+//                pc.getProjectWithMaxFound2().getBudget(),
+//                pc.getProjectWithMaxFound2().getFoundByProject());
         //        pc.getProjectsOrderByBudgetDESC().stream()
 //                .forEach(projectsFounds -> System.out.printf("Acronim: %-25s Budget: %20.2f PLN \n",
 //                        projectsFounds.getAcronim(),
